618c177a6aa91c3a3e046678afdb867f4a5b301e
Roman <slyusarrv@yandex.ru>
2022-01-28 13:05:09 +0500

    added the ability to set a discount for products of the same category

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -133,6 +134,14 @@ class CategoryUpdate(AdminAccessMixin, UpdateView):
     # fields = '__all__'
     form_class = ProductCategoryForm
     success_url = reverse_lazy('adminapp:categories')
+
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set.update(price=F('price') * (
+                        1-discount/100))
+        return super().form_valid(form)
 
 
 # @user_passes_test(lambda u: u.is_superuser)
