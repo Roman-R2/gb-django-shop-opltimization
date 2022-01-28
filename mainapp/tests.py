290b1adda27f47fbf296d096b879d762ae51bb6e
Roman <slyusarrv@yandex.ru>
2022-01-28 16:16:05 +0500

    added smoke tests

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
-# Create your tests here.
+from mainapp.models import Category, Product
+
+
+class MainAppSmokeTest(TestCase):
+    HTTP_200_OK = 200
+
+    def setUp(self):
+        category = Category.objects.create(
+            name='cat_1',
+            slug='cat_1',
+            description='test_cat_description',
+        )
+        for i in range(10):
+            Product.objects.create(
+                category=category,
+                slug=f'prod_{i}',
+                name=f'prod_{i}',
+                description='test_description',
+                price=100,
+            )
+
+    def test_mainapp_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.HTTP_200_OK)
+
+    def test_categories_urls(self):
+        for cat in Category.objects.all():
+            response = self.client.get(f'/products/{cat.slug}/')
+            self.assertEqual(response.status_code, self.HTTP_200_OK)
+
+    def test_products_urls(self):
+        for prod in Product.objects.all():
+            response = self.client.get(f'/product/{prod.slug}/')
+            self.assertEqual(response.status_code, self.HTTP_200_OK)
+
+    def tearDown(self):
+        pass
--- shop/settings.py
+++ shop/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 SERVER_ENV = os.getenv('SERVER_ENV')
 
 # Хосты, по которым можно зайти на сайт (* - с любого хоста)
